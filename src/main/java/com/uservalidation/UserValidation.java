d967cc070efc507fba4fa64970a875f59b858a1b
gaurav5991 <gauravenggsharma2017@gmail.com>
2020-09-30 16:34:52 +0530

    [Gaurav]REFACTORED: Validating First Name Against TestCase

--- src/main/java/com/uservalidation/UserValidation.java
+++ src/main/java/com/uservalidation/UserValidation.java
@@ -1,9 +1,18 @@
 package com.uservalidation;
 
+import java.util.regex.Pattern;
+
 public class UserValidation {
+
+    private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[A-Za-z][a-zA-Z]+$";
 
     public void printWelcomeMessage()
     {
         System.out.println("Welcome to User Validation Program");
+    }
+
+    public boolean validateFirstname(String first_name)
+    {
+        return Pattern.matches(FIRST_NAME_PATTERN, first_name);
     }
 }
--- src/test/java/com/uservalidation/userValidationTest.java
+++ src/test/java/com/uservalidation/userValidationTest.java
@@ -1,13 +1,39 @@
 package com.uservalidation;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class userValidationTest {
 
     @BeforeClass
-    public static void beforeClass() throws Exception {
+    public static void beforeClass() {
         UserValidation validate = new UserValidation();
         validate.printWelcomeMessage();
 
+    }
+
+    @Test
+    public void givenFirstName_When_ProperShouldReturnTrue()
+    {
+        UserValidation validate = new UserValidation();
+        boolean result = validate.validateFirstname("Gaurav");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstName_When_Short_ShouldReturnFalse()
+    {
+        UserValidation validate = new UserValidation();
+        boolean result = validate.validateFirstname("Ga");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenFirstName_have_special_Character_ShouldReturnFalse()
+    {
+        UserValidation validate = new UserValidation();
+        boolean result = validate.validateFirstname("G@urav");
+        Assert.assertFalse(result);
     }
 }
